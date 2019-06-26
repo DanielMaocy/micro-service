5fb5d7a96971bdb7c1d5f80ad8cf9e690d0344d9
maocy <345106328@qq.com>
2019-06-26 11:57:03 +0800

    spring cloud bus

--- demo-01/config-server/src/main/java/com/maocy/ConfigServerApplication.java
+++ demo-01/config-server/src/main/java/com/maocy/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.maocy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer // 开启Spring Cloud Config的服务端功能
 @SpringBootApplication
 public class ConfigServerApplication {
--- demo-01/rabbitmq-service/src/main/java/com/maocy/RabbitmqServiceApplication.java
+++ demo-01/rabbitmq-service/src/main/java/com/maocy/RabbitmqServiceApplication.java
@@ -3,10 +3,12 @@ package com.maocy;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RabbitmqServiceApplication {
 	
+	@Bean
 	public Queue testQueue() {
 		return new Queue("test");
 	}
